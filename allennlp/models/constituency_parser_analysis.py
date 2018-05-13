3db889358b70a59676b38369b0dc070c301cb2dd
Mark Neumann <markng@allenai.org>
2018-05-13 14:27:25 -0700

    fix span mask

--- allennlp/models/constituency_parser_analysis.py
+++ allennlp/models/constituency_parser_analysis.py
@@ -208,7 +208,7 @@ class SpanConstituencyParserAnalysis(Model):
         if span_labels is not None:
             loss = 0.0
             loss = sequence_cross_entropy_with_logits(logits, span_labels, span_mask)
-            self.tag_accuracy(class_probabilities, span_labels, mask)
+            self.tag_accuracy(class_probabilities, span_labels, span_mask)
             output_dict["loss"] = loss
 
         # The evalb score is expensive to compute, so we only compute
